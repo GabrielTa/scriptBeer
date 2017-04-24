4dafe524fe27d0942b5349a633b6d229f1bfb6cd
Rishi Speets <rishispeets@gmail.com>
2017-04-24 10:30:03 +0200

    changed footer

---
+++ images/scriptBeer_files/script.js
@@ -0,0 +1,29 @@
+$(function(){
+        if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+          $('#ios-notice').removeClass('hidden');
+          $('.parallax-container').height( $(window).height() * 0.5 | 0 );
+        } else {
+          $(window).resize(function(){
+            var parallaxHeight = Math.max($(window).height() * 0.7, 200) | 0;
+            $('.parallax-container').height(parallaxHeight);
+          }).trigger('resize');
+        }
+      });
+
+
+$(function(){
+        if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+          $('#ios-notice').removeClass('hidden');
+          $('.parallax-header').height( $(window).height() * 0.5 | 0 );
+        } else {
+          $(window).resize(function(){
+            var parallaxHeight = Math.max($(window).height() * 1, 200) | 0;
+            $('.parallax-header').height(parallaxHeight);
+          }).trigger('resize');
+        }
+      });
+
+
+
+
+
