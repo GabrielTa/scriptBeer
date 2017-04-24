af3dbeb4bf6a5dc8da7e36d9f71f6e94b1c9d482
Rishi Speets <rishispeets@gmail.com>
2017-04-24 14:25:12 +0200

    add gmaps marker

--- js/script.js
+++ js/script.js
@@ -23,6 +23,17 @@ $(function(){
         }
       });
 
+function initMap() {
+        var uluru = {lat: 52.341202, lng: 4.823109};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
 
 
 
